9a85c91a242707fd007071b628e101b684103808
shaikhzaid01 <shaikhzaid468@gmail.com>
2026-09-02 12:30:32 +0530

    feat: complete tv_template_v2.0.1 with dynamic menus, multi-language support, and luxury UI

---
+++ assets/js/menuData.js
@@ -0,0 +1,65 @@
+window.MENU_DATA = [
+    {
+        id: 'hotel_menu',
+        name: 'Hotel Menu',
+        icon: 'assets/images/icons/hotelinfo.png',
+        status: 'show',
+        sub_menus: [
+            {
+                id: 'hotel_info',
+                name: 'Hotel Info',
+                icon: 'assets/images/icons/hotelinfo.png',
+                status: 'show'
+            },
+            {
+                id: 'room_info',
+                name: 'Room Info',
+                icon: 'assets/images/icons/amenities.png',
+                status: 'show'
+            },
+            {
+                id: 'amenities',
+                name: 'Amenities',
+                icon: 'assets/images/icons/roomservice.png',
+                status: 'show'
+            }
+        ]
+    },
+    {
+        id: 'apps',
+        name: 'Applications',
+        icon: 'assets/images/icons/apps.png',
+        status: 'show'
+    },
+    {
+        id: 'language',
+        name: 'Language',
+        icon: 'assets/images/icons/languages.png',
+        status: 'show'
+    },
+    {
+        id: 'livetv',
+        name: 'Live TV',
+        icon: 'assets/images/icons/livetv.png',
+        status: 'show'
+    },
+    {
+        id: 'interactive_services',
+        name: 'Interactive Services',
+        icon: 'assets/images/icons/roomservice.png',
+        status: 'show',
+        sub_menus: []
+    },
+    {
+        id: 'ourcity',
+        name: 'Our City',
+        icon: 'assets/images/icons/ourcity.png',
+        status: 'show'
+    },
+    {
+        id: 'screen_cast',
+        name: 'Screen Cast',
+        icon: 'assets/images/icons/cast.png',
+        status: 'show'
+    }
+];
